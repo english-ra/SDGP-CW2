63434f86bfff0a3fe00cbf3d1e763e5a33848574
Nathan-nganga <nathan4nganga@gmail.com>
2022-03-24 15:02:53 +0000

    Created the connectdb class. Created the Createusertable  class with constraints.

---
+++ src/ConnectDB.java
@@ -0,0 +1,24 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author natty
+ */
+public class ConnectDB {
+    public static Connection getConnection() {
+        try {
+            Class.forName("org.sqlite.JDBC");
+            String url = "jdbc:sqlite:test.db";
+            Connection conn = DriverManager.getConnection(url);
+            //JOptionPane.showMessageDialog(null, "Connection Established");
+            return conn;
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null, e);
+            return null;
+        }
+    }
+
+}
+
