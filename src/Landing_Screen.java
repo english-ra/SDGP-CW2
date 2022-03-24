b2e35b8b7c57a3d6e3faeeba96ac6d358c83e212
Reece English <englishra.dev@icloud.com>
2022-03-24 20:19:09 +0000

    - Converted the Landing_Screen to use SpringLayout

--- src/Landing_Screen.java
+++ src/Landing_Screen.java
@@ -6,16 +6,15 @@ import java.util.ArrayList;
 
 public class Landing_Screen extends JPanel {
     private JFrame mainFrame;
-    private JLabel logoLabel;
+    private TitleLabel logoLabel;
     private MainButton registerButton;
     private MainButton signInButton;
 
+    private SpringLayout layout;
+
     private ArrayList uiFlow = new ArrayList<JPanel>();
 
-    GridLayout layout;
-
     public Landing_Screen(JFrame mainFrame) {
-        this.signInButton = signInButton;
         this.mainFrame = mainFrame;
         uiFlow.add(this);
 
@@ -29,38 +28,41 @@ public class Landing_Screen extends JPanel {
 
     private void configureRootPanel() {
         this.setBackground(new Color(20, 54, 66));
+        this.setPreferredSize(new Dimension(350, 750));
 
-        layout = new GridLayout(2, 1, 25, 300);
+        layout = new SpringLayout();
         this.setLayout(layout);
     }
 
 
     private void configureLogoLabel() {
         // Configure the logo label
-        logoLabel = new JLabel();
-        logoLabel.setText("Language Practice");
-        logoLabel.setForeground(Color.WHITE);
-        logoLabel.setFont(new Font("Arial Black", Font.BOLD, 28));
-        logoLabel.setHorizontalAlignment(SwingConstants.CENTER);
+        logoLabel = new TitleLabel("Language\nPractice");
         this.add(logoLabel);
+
+        layout.putConstraint(SpringLayout.NORTH, logoLabel, 20, SpringLayout.NORTH, this);
+        layout.putConstraint(SpringLayout.WEST, logoLabel, 20, SpringLayout.WEST, this);
+        layout.putConstraint(SpringLayout.EAST, logoLabel, -20, SpringLayout.EAST, this);
     }
 
 
     private void configureButtons() {
-        JPanel buttonPanel = new JPanel();
-        buttonPanel.setBackground(Colours.mainBG);
 
-        // Configure the sign in button
+        // Configure the sign-in button
         signInButton = new MainButton("Sign In");
-        signInButton.setPreferredSize(new Dimension(mainFrame.getWidth() - 10, 50));
-        buttonPanel.add(signInButton);
+        this.add(signInButton);
 
         // Configure the register button
         registerButton = new MainButton("Register");
-        registerButton.setPreferredSize(new Dimension(mainFrame.getWidth() - 10, 50));
-        buttonPanel.add(registerButton);
+        this.add(registerButton);
 
-        this.add(buttonPanel);
+        layout.putConstraint(SpringLayout.SOUTH, registerButton, -50, SpringLayout.SOUTH, this);
+        layout.putConstraint(SpringLayout.WEST, registerButton, 20, SpringLayout.WEST, this);
+        layout.putConstraint(SpringLayout.EAST, registerButton, -20, SpringLayout.EAST, this);
+
+        layout.putConstraint(SpringLayout.SOUTH, signInButton, -10, SpringLayout.NORTH, registerButton);
+        layout.putConstraint(SpringLayout.WEST, signInButton, 20, SpringLayout.WEST, this);
+        layout.putConstraint(SpringLayout.EAST, signInButton, -20, SpringLayout.EAST, this);
     }
 
 
