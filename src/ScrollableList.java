51f7f5be269d248ca4142fc9198fb708fab324d2
Reece English <englishra.dev@icloud.com>
2022-03-29 19:09:45 +0100

    - Set the text in the ScrollableList to the center - Changed the selected list items colour

--- src/ScrollableList.java
+++ src/ScrollableList.java
@@ -6,9 +6,17 @@ public class ScrollableList extends JScrollPane {
 
     public ScrollableList(String[] items) {
         super();
+
+        // Configure the list
         list = new JList(items);
         this.setViewportView(list);
 
+        // Configure the lists cells
         list.setFixedCellHeight(50);
+        list.setSelectionBackground(Colours.mainFG);
+
+        // Configure the text in the list
+        DefaultListCellRenderer renderer = (DefaultListCellRenderer) list.getCellRenderer();
+        renderer.setHorizontalAlignment(SwingConstants.CENTER);
     }
 }
